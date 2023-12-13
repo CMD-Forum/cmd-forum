fa30bcd181361e6288470c499f9473677805e3ed
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2023-12-13 20:52:23 +0000

    Fixed 'react-icons' breaking build

--- app/ui/components/dropdown.tsx
+++ app/ui/components/dropdown.tsx
@@ -3,7 +3,6 @@
 import React, { useState } from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import { IconType } from 'react-icons';
 import { UserIcon, 
   DocumentTextIcon, 
   Cog6ToothIcon, 
