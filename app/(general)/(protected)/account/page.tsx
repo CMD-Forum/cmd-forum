7058ebfc16f9ecd3ba24b30c4bef67b6573c3886
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2024-03-03 00:37:21 +0000

    '/account' now redirects to the appropiate page in '/user'

--- app/(general)/(protected)/account/page.tsx
+++ app/(general)/(protected)/account/page.tsx
@@ -1,57 +1,14 @@
-import { auth } from "@/auth";
-import { prisma } from "@/app/(general)/lib/db";
-import ProfileMain from "@/app/(general)/ui/components/account/ProfileMain";
-import { redirect } from 'next/navigation';
-import PostListByUser from "../../ui/components/posts/post_list_custom";
+"use client";
 
-const Account = async () => {
+import { useRouter } from "next/navigation";
+import { useSession } from "next-auth/react";
 
-    const session = await auth();
-  
-    const p_user = await prisma.user.findUnique({ where: { id: session?.user.id }})
+const Account = () => {
 
-    if (p_user) {
+    const { data: session, update } = useSession();
+    const router = useRouter();
 
-        const p_user_post_count = await prisma.post.count({ where: { authorId: p_user?.id }})  
-
-        return (
-
-            <div>
-                {session ?
-                <>
-                    <ProfileMain 
-                        username={p_user.username} 
-                        image={p_user.image} 
-                        description={p_user.description} 
-                        createdAt={p_user?.createdAt} 
-                        postCount={p_user_post_count} 
-                    />
-
-
-                    <p className="text-gray-300 font-bold antialiased w-full text-center md:text-left mt-4">{p_user.username}&apos;{p_user.username.endsWith("s") ? "" : "s"} Posts</p>   
-
-                    <hr className="mt-2" />
-
-                    <div className="flex flex-col gap-4 mt-4">
-                        <PostListByUser username={p_user.username} />                             
-                    </div>
-          
-                </>
-                :
-                <h1 className="header">Looks like you&apos;re account has been logged out.</h1>
-                }
-            </div>
-
-        )
-
-    } else {
-        return (
-            <h1 className="header">This account doesn&apos;t exist.</h1>
-        );
-    }
-    
-
-
+    router.push(`/user/${session?.user.username}`);
 
 };
 
