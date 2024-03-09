0685bdf31af3234ede801c217d595990c0e45524
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2024-03-09 23:54:08 +0000

    :ambulance: Added loading page to (auth) to try and fix prerender error.

---
+++ app/(auth)/loading.tsx
@@ -0,0 +1,7 @@
+export default function Loading() {
+
+    return (
+        <div className="bg-border border-1 border-border absolute translate-x-1/2 translate-y-1/2 p-6 font-bold">Loading...</div>
+    );
+
+}
