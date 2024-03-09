6a2c52687b0ed7d667eb1ad99249aea38c099a95
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2024-03-09 23:02:56 +0000

    :bug: Updated `seed.ts` to bring it in line with new database.

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -7,28 +7,30 @@ async function main() {
 
   const communities = [
 
-    { name: "general", image: "https://placehold.co/400", public: true, display_name: "General" },
-    { name: "meta", image: "https://placehold.co/400", public: true, display_name: "Meta" },
-    { name: "television", image: "https://placehold.co/400", public: true, display_name: "Television" },
-    { name: "movies", image: "https://placehold.co/400", public: true, display_name: "Movies" },
-    { name: "music", image: "https://placehold.co/400", public: true, display_name: "Music" },
+    { id: "1", name: "general", image: "https://placehold.co/400", public: true, display_name: "General" },
+    { id: "2", name: "meta", image: "https://placehold.co/400", public: true, display_name: "Meta" },
+    { id: "3", name: "television", image: "https://placehold.co/400", public: true, display_name: "Television" },
+    { id: "4", name: "movies", image: "https://placehold.co/400", public: true, display_name: "Movies" },
+    { id: "5", name: "music", image: "https://placehold.co/400", public: true, display_name: "Music" },
 
   ];
 
   const users = [
 
-    { email: "johndoe@example.com", username: "username1", password: await hash("12345678", 10), name: "John Doe" },
-    { email: "janedoe@example.com", username: "username2", password: await hash("12345678", 10), name: "Jane Doe" },
+    { id: "1", email: "johndoe@example.com", username: "username1", password: await hash("12345678", 10), name: "John Doe" },
+    { id: "2", email: "janedoe@example.com", username: "username2", password: await hash("12345678", 10), name: "Jane Doe" },
+    { id: "3", email: "user@testing.com", username: "Testing521", password: await hash("12345678", 10), name: "Testing521" },
+    { id: "4", email: "hello@howareyou.com", username: "HowAreYou", password: await hash("12345678", 10), name: "HowAreYou" },
 
   ];
 
   const posts = [
     
-    { communityId: 1, title: "Look at my code!", content: "**_Here's my code!_**", tagline: "Made some code today.", public: true, authorId: 1, imageurl: "/images/uploaded/code.png", imagealt: "My Code!", },
-    { communityId: 2, title: "The Cliffs of Moher", content: "# The Cliffs of Moher \n Here is the cliffs of moher! \n ## The Cliffs History \n Loreum Ipsum...", tagline: "Post Tagline 2", public: true, authorId: 2, imageurl: "https://images.unsplash.com/photo-1530538095376-a4936b35b5f0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", imagealt: "The Cliffs of Moher" },
-    { communityId: 3, title: "So, what did you think of last nights episode?", content: "I think it was great, how about you?", tagline: "It was great, I'd say.", public: true, authorId: 2, imageurl: "", imagealt: "", },
-    { communityId: 2, title: "Followup to my code post.", content: "## Well, I think it was great. \n Yeah, you're wrong and I'm right.", tagline: "I'm right about it.", public: true, authorId: 1, imageurl: "/images/uploaded/code.png", imagealt: "I was right!", },
-    { communityId: 2, title: "I think \"Snakes on a Plane\" was just okay.", content: "# Hah, just kidding. \n It's actually great.", tagline: "Look inside.", public: true, authorId: 2, imageurl: "", imagealt: "", },
+    { communityId: "1", title: "Look at my code!", content: "**_Here's my code!_**", tagline: "Made some code today.", public: true, authorId: "1", imageurl: "/images/uploaded/code.png", imagealt: "My Code!", },
+    { communityId: "1", title: "The Cliffs of Moher", content: "# The Cliffs of Moher \n Here is the cliffs of moher! \n ## The Cliffs History \n Loreum Ipsum...", tagline: "Post Tagline 2", public: true, authorId: "2", imageurl: "https://images.unsplash.com/photo-1530538095376-a4936b35b5f0?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D", imagealt: "The Cliffs of Moher" },
+    { communityId: "3", title: "So, what did you think of last nights episode?", content: "I think it was great, how about you?", tagline: "It was great, I'd say.", public: true, authorId: "2", imageurl: null, imagealt: null, },
+    { communityId: "2", title: "Feature suggestion", content: "What if we implement this feature, using the code above?", tagline: "My new feature request.", public: true, authorId: "3", imageurl: "/images/uploaded/code.png", imagealt: "The code in question.", },
+    { communityId: "5", title: "I think \"Snakes on a Plane\" was just okay.", content: "# Hah, just kidding. \n It's actually great.", tagline: "Look inside.", public: true, authorId: "4", imageurl: null, imagealt: null, },
   
   ];
 
