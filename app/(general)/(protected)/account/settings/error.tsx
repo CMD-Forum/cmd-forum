a908b4072f9e3e5e2a9f560863a134db48ae53ab
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2024-05-04 00:14:19 +0100

    :fire: Removed magic `layout.tsx` that appeared when using Git.

---
+++ app/(general)/(protected)/account/settings/error.tsx
@@ -0,0 +1,21 @@
+"use client";
+
+export default function Error({
+    error,
+    reset,
+}: {
+    error: Error & { digest?: string }
+    reset: () => void
+}) {
+    return (
+        <div className="p-6 pt-12 lg:pb-12 lg:p-12 lg:px-48">
+            <div className='flex flex-col items-center justify-center w-full relative group transition-all bg-card h-[174px] rounded-md px-5 py-5'>
+                <p className='text-center !text-white font-medium antialiased w-full'>Sorry, an error occurred.</p>
+                <p className='text-center !text-white font-medium antialiased w-full'>{ error.digest }</p>
+                <div className='flex gap-4 w-full items-center justify-center mt-4'>
+                    <button className='navlink' onClick={() => reset()} type='button'>Reload</button>
+                </div>
+            </div>            
+        </div>
+    );
+}
