fd9e9fd4f28604d13c9198227fbb481643ca9825
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2024-06-01 23:11:15 +0100

    chore(ui): error page displays message if in development
    Note that only a digest will be displayed in production.

--- app/error.tsx
+++ app/error.tsx
@@ -7,11 +7,14 @@ export default function Error({
     error: Error & { digest?: string }
     reset: () => void
 }) {
+
+    const env = process.env.NODE_ENV
+
     return (
         <div className="p-6 pt-12 lg:pb-12 lg:p-12 lg:px-48">
             <div className='flex flex-col items-center justify-center w-full relative group transition-all bg-card h-[174px] rounded-md px-5 py-5'>
-                <p className='text-center !text-white font-medium antialiased w-full'>Sorry, an error occurred.</p>
-                <p className='text-center !text-white font-medium antialiased w-full'>{ error.digest }</p>
+                <p className='text-center subtitle'>Sorry, an error occurred.</p>
+                <p className='text-center subtitle'>{ env === "development" || env === "test" ? error.message : error.digest }</p>
                 <div className='flex gap-4 w-full items-center justify-center mt-4'>
                     <button className='navlink' onClick={() => reset()} type='button'>Reload</button>
                 </div>
