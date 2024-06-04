ad395f42c81effa6d85d57080a679ccd2f313c12
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2024-06-04 02:10:40 +0100

    fix(db): temporarily fixes `globalThis` error

--- app/(general)/lib/db.ts
+++ app/(general)/lib/db.ts
@@ -4,6 +4,8 @@ declare global {
   var prisma: PrismaClient | undefined;
 }
 
+// eslint-disable-next-line no-undef
 export const prisma = globalThis.prisma || new PrismaClient();
 
+// eslint-disable-next-line no-undef
 if (process.env.NODE_ENV !== 'production') globalThis.prisma = prisma
