25ec4fc32cb0a15ecc523d34328eb115cc1a73f5
CMD-Forum <59456097+CMD-Forum@users.noreply.github.com>
2024-06-06 00:07:40 +0100

    feat(ui): add unimplemented page for respective features

--- app/(auth)/forgot-password/page.tsx
+++ app/(auth)/forgot-password/page.tsx
@@ -1,17 +1,7 @@
-import { Metadata } from 'next';
 import Link from "next/link";
- 
-export const metadata: Metadata = {
-  title: 'About - CMD',
-};
 
-export default function AboutPage() {
-  return (
-    <main className="flex min-h-screen flex-col w-full">
-      <div className="error flex flex-col w-full">
-        <div className="flex flex-col border-0 border-border p-6 pt-12 lg:pb-12 lg:p-12 lg:px-48 bg-card mt-8 md:mt-0">
-          <h1 className="header">About CMD/&gt;</h1>
-        </div>
+export default function ForgotPasswordPage() {
+    return (
         <div className="p-6 pt-12 lg:pb-12 lg:p-12 lg:px-48">
             <div className='flex flex-col items-center justify-center w-full relative group transition-all bg-card h-[174px] rounded-md px-5 py-5'>
                 <p className='text-center text-gray-300 font-medium antialiased w-full'>Sorry, this feature is under development.</p>
@@ -20,7 +10,5 @@ export default function AboutPage() {
                 </div>
             </div>            
         </div>
-      </div>
-    </main>
-  );
+    );
 }
--- app/(auth)/logout/page.tsx
+++ app/(auth)/logout/page.tsx
@@ -1,6 +1,5 @@
 import { Metadata } from 'next';
-
-import { signOut } from "@/auth"
+import Link from 'next/link';
  
 export const metadata: Metadata = {
   title: 'Logout - CMD',
@@ -10,16 +9,10 @@ export default function SignOutPage() {
   return (
     <div className="p-6 pt-12 lg:pb-12 lg:p-12 lg:px-48">
       <div className='flex flex-col items-center justify-center w-full relative group transition-all bg-card h-[174px] rounded-md px-5 py-5'>
-        <p className='text-center !text-white font-medium antialiased w-full'>Are you sure you want to sign out?</p>
-        <form
-          action={async (formData) => {
-            "use server"
-            await signOut();
-          }}
-          className=""
-        >
-          <button type="submit" className="navlink-emphasis mt-4">Sign out</button>
-        </form>
+          <p className='text-center text-gray-300 font-medium antialiased w-full'>Sorry, this feature is under development.</p>
+          <div className='flex gap-4 w-full items-center justify-center mt-4'>
+              <Link className='navlink' href={"/"}>Home</Link>
+          </div>
       </div>            
     </div>
   )
